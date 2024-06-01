cc1fc214655c4277b1382de669b831e512e3513d
Peznc <peznc.810@gmail.com>
2024-06-01 17:03:25 +0800

    chore(config): update tailwind configuration for init theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    fontFamily: {
-      'megrim': ['Megrim', 'system-ui'],
-    },
     container: {
       center: true,
     },
@@ -15,16 +12,7 @@ export default {
       'lg': '1024px',
       'xl': '1280px',
     },
-    extend: {
-      colors: {
-        'primary': '#080F41',
-        'primary-light': '#57B9E9',
-        'success': '#7e5bef',
-        'blue': '#8CAFF6',
-        'warning': '#F3A549',
-        'danger': '#F6AAA9',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
