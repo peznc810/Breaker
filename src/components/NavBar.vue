b4b174631be2df1dd8b46b0ae0aa0f2521ada14d
Peznc <peznc.810@gmail.com>
2024-06-05 00:45:42 +0800

    feat(page): update preliminary layout of the page.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <div class="flex h-full justify-between">
+      <div
+        class="grid h-[50px] w-60 place-content-center border-b bg-gray-700 text-white"
+      >
+        <div class="flex-shrink-0">IF SHOP</div>
+      </div>
+      <div class="flex-1 bg-white px-3">
+        <div class="flex h-full items-center justify-end">I'm NavBar</div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <aside>
+    <ul class="absolute bottom-0 top-[50px] w-60 overflow-y-auto">
+      <li v-for="menu in menus" :key="menu.name">
+        <RouterLink
+          :to="menu.path"
+          active-class="bg-gray-600 shadow-sm"
+          class="inline-block w-full px-4 py-3 hover:bg-gray-600"
+          >{{ menu.name }}</RouterLink
+        >
+      </li>
+      <!-- <li >mfkdslf</li> -->
+    </ul>
+  </aside>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref, computed } from 'vue'
+import { RouterLink, useRoute } from 'vue-router'
+
+const route = useRoute()
+const routeName = computed(() => route.name)
+
+const menus = reactive([
+  { name: '總覽', path: '/', icon: '' },
+  { name: '訂單管理', path: '/orderManagement', icon: '' },
+  { name: '商品管理', path: '/productManagement', icon: '' },
+  { name: '報表及分析', path: '/report', icon: '' }
+])
+</script>
+
+<style scoped></style>
