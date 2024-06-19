7c5a2aa7c4ac59b82e99c996204a799fb9babe37
Peznc <peznc.810@gmail.com>
2024-06-19 11:17:21 +0800

    chroe(confug): update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
-import './assets/main.css'
-import ElementPlus from 'element-plus'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
 import vSvg from './directives/svg'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import './assets/main.css'
 
 const app = createApp(App)
 
+/* custom directives */
 app.directive('svg', vSvg)
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus)
+/* ElementPlus */
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
