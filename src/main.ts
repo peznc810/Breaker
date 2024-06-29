b97ea0377696cf544e105d249ce0ee53d57aeea8
Peznc <peznc.810@gmail.com>
2024-06-29 22:05:07 +0800

    fix: 移除element-icon，新增vue-icon

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+
 import App from './App.vue'
 import router from './router'
 import vSvg from './directives/svg'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import i18n from './i18n'
-
+// styles
 import './assets/main.css'
+// oh-vue-icons
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { HiChevronDown } from "oh-vue-icons/icons";
+
+addIcons(HiChevronDown);
 
 const app = createApp(App)
 
@@ -15,9 +20,7 @@ app.directive('svg', vSvg) // custom directives
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
-// ElementPlus Icon !!目前引入有問題!!
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
+
+app.component('v-icon', OhVueIcon)
 
 app.mount('#app')
