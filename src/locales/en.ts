1fcb0dd7819c0b08ea6fa1cfa895810091d65170
Peznc <peznc.810@gmail.com>
2024-07-04 15:55:48 +0800

    feat(lang): 新增翻譯

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,6 +1,28 @@
 export default {
   hello: 'Hello',
-  morning: 'morning',
-  afternoon: 'afternoon',
-  evening: 'evening',
+  morning: 'Good morning',
+  afternoon: 'Good afternoon',
+  evening: 'Good evening',
+  am: 'AM',
+  pm: 'PM',
+  nav: {
+    profile: 'Profile',
+    changePassword: 'Change Password',
+    logout: 'Logout',
+  },
+  overview: {
+    unRead: 'Unread messages',
+    unShipped: 'Orders not shipped',
+    unpaid: 'Orders unpaid',
+    operationalMsg: "Here is today's store operational overview",
+    dataEndTime: 'Data as of',
+  },
+  moreData: 'more data',
+  select: {
+    plzSelect: 'Please select',
+    today: 'Today',
+    yesterday: 'Yesterday',
+    sevenDaysBefore: 'Last 7 days',
+    thirtyDaysBefore: 'Last 30 days',
+  },
 }
--- src/locales/zh-router.ts
+++ src/locales/zh-router.ts
@@ -1,3 +1,6 @@
 export default {
   dashboard: '總覽',
+  orderManagement: '訂單管理',
+  productManagement: '商品管理',
+  report: '報表及分析',
 }
