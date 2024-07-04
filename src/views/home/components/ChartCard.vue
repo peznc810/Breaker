58216629631a8dadd6db25c4e4b6cc1cceedc9dd
Peznc <peznc.810@gmail.com>
2024-07-04 16:31:18 +0800

    refactor(declaration): 調整定義及檔案位置

---
+++ src/views/home/components/ChartCard.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+  import Card from '@/components/Card.vue'
+  import type { ChartCardProps } from '@/@types/view/home'
+
+  const props = defineProps<ChartCardProps>()
+</script>
+
+<template>
+  <Card :data="props.data" class="hover:cursor-pointer hover:shadow-lg hover:shadow-black/10">
+    <template #default="{ row }">
+      <div class="flex h-full flex-col items-center justify-center gap-5">
+        <v-icon :name="row.icon" scale="1.3" fill="#5087e6" />
+        <div class="text-xl font-medium">{{ row.count }}</div>
+        <div>{{ row.title }}</div>
+      </div>
+    </template>
+  </Card>
+</template>
+
+<style scoped></style>
