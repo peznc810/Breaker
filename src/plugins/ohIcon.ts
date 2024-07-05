5b2900d9e9f4fe222aa879a7480ab74a97224144
Peznc <peznc.810@gmail.com>
2024-07-05 17:42:07 +0800

    feat(view): 新增總覽切版

--- src/plugins/ohIcon.ts
+++ src/plugins/ohIcon.ts
@@ -1,6 +1,27 @@
-import { HiChevronDown, MdLanguage } from "oh-vue-icons/icons";
+import {
+  HiChevronDown,
+  MdLanguage,
+  RiMessage2Line,
+  BiCreditCard2Back,
+  HiClipboardList,
+  BiGrid1X2,
+  LaShoppingBagSolid,
+  LaUserSolid,
+  FcComboChart,
+  RiLoader2Fill,
+  RiShareBoxLine,
+} from 'oh-vue-icons/icons'
 
 export const ohIcon = [
-  HiChevronDown, 
-  MdLanguage,
+  HiChevronDown, // 向下箭頭
+  MdLanguage, // 語言
+  RiMessage2Line, // 訊息
+  BiCreditCard2Back, // 信用卡
+  HiClipboardList, // 表單
+  BiGrid1X2, // 總覽
+  LaShoppingBagSolid, // 商品
+  LaUserSolid, // 會員
+  FcComboChart, // 數據
+  RiLoader2Fill, // loading
+  RiShareBoxLine, // 開分頁
 ]
--- src/views/home/components/ChartCard.vue
+++ src/views/home/components/ChartCard.vue
@@ -1,20 +1,80 @@
 <script lang="ts" setup>
-  import Card from '@/components/Card.vue'
-  import type { ChartCardProps } from '@/@types/view/home'
+  import { ref } from 'vue'
+  import { RouterLink } from 'vue-router'
+  import Chart from './Chart.vue'
 
-  const props = defineProps<ChartCardProps>()
+  const props = defineProps<{
+    data: any
+    class?: string
+  }>()
+
+  const transActive = ref(1)
+  const totalOrdersActive = ref(1)
+
+  const handleActive = (index: number, option: number) => {
+    if (index === 0) {
+      transActive.value = option
+    } else {
+      totalOrdersActive.value = option
+    }
+  }
 </script>
 
 <template>
-  <Card :data="props.data" class="hover:cursor-pointer hover:shadow-lg hover:shadow-black/10">
-    <template #default="{ row }">
-      <div class="flex h-full flex-col items-center justify-center gap-5">
-        <v-icon :name="row.icon" scale="1.3" fill="#5087e6" />
-        <div class="text-xl font-medium">{{ row.count }}</div>
-        <div>{{ row.title }}</div>
+  <div
+    v-for="(item, index) in props.data"
+    :key="item.title"
+    class="w-full space-y-3 rounded-lg bg-white p-6 shadow-sm transition-shadow duration-300"
+  >
+    <!-- Header -->
+    <div class="flex items-center justify-between">
+      <div class="text-md font-normal">{{ item.title }}</div>
+      <div class="btn">
+        <div v-show="index === 0">
+          <el-button-group class="ml-6">
+            <el-button
+              :class="transActive === 1 && 'bg-blue-400 text-white'"
+              @click="handleActive(index, 1)"
+              size="mini"
+              >{{ $t('overview.transactionTotal') }}</el-button
+            >
+            <el-button
+              :class="transActive === 2 && 'bg-blue-400 text-white'"
+              @click="handleActive(index, 2)"
+              size="mini"
+              >{{ $t('overview.totalSales') }}</el-button
+            >
+          </el-button-group>
+        </div>
+        <div v-show="index === 1">
+          <el-button-group class="ml-6">
+            <el-button
+              :class="totalOrdersActive === 1 && 'bg-blue-400 text-white'"
+              @click="handleActive(index, 1)"
+              size="mini"
+              >{{ $t('overview.totalCompletedOrders') }}</el-button
+            >
+            <el-button
+              :class="totalOrdersActive === 2 && 'bg-blue-400 text-white'"
+              @click="handleActive(index, 2)"
+              size="mini"
+              >{{ $t('overview.totalOrder') }}</el-button
+            >
+          </el-button-group>
+          <RouterLink to="/orderManagement" class="ml-2">
+            <el-button icon="ri-share-box-line" size="mini" />
+          </RouterLink>
+        </div>
       </div>
-    </template>
-  </Card>
+    </div>
+
+    <div class="text-lg font-bold">NT$ {{ item.count }}</div>
+
+    <!-- Chart -->
+    <div class="mt-4">
+      <Chart />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
