63965b4d8a9635ce46d4855a857121fb65fe20bc
Peznc <peznc.810@gmail.com>
2024-07-06 00:58:28 +0800

    feat(component): 新增客製化

--- src/@types/components/table.d.ts
+++ src/@types/components/table.d.ts
@@ -1,10 +1,13 @@
-interface Columns {
+export interface Columns {
   label: string
   prop: string
+  type?: 'currency'
+  width?: string
 }
 
 export interface TableProps {
   data?: any[]
   columns?: Columns[]
   class?: string
+  type?: 'default' | 'index' | 'checkbox'
 }
