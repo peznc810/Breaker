5bb5f3de5237519b3bc8acdc69a239eb68084835
Peznc <peznc.810@gmail.com>
2024-07-08 21:41:38 +0800

    feat(component): 新增卡片組件

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import type { CardProps } from '@/@types/components/card'
+  const props = defineProps<CardProps>()
+</script>
+
+<template>
+  <div
+    v-for="(item, index) in props.data"
+    :key="index"
+    class="w-full rounded-sm bg-white p-4 shadow-md transition-shadow duration-300"
+    :class="props.class"
+  >
+    <slot :row="item" />
+  </div>
+</template>
